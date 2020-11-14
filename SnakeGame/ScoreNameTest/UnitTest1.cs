dc12083ed628180a2d919e403e6cdf970b0b2da7
Jonathan <51481339+E5THEEM@users.noreply.github.com>
2020-11-15 02:58:12 +0800

    ScoreNameTest

---
+++ SnakeGame/ScoreNameTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ScoreNameTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
